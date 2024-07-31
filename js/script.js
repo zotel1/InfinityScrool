9f15b6be3fb873138c3b4cb908c0975e4bb9e0c7
zotel1 <zotelsigel@gmail.com>
2024-07-31 16:24:59 -0300

    agregamos comentarios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-const container = document.querySelector('.container')
+// Selecciona el elemento con la clase 'container'
+const container = document.querySelector('.container');
 
+// Agrega un evento de desplazamiento (scroll) a la ventana
 window.addEventListener('scroll', () => {
-    const {scrollHeight, clientHeight, scrollTop} =  document.documentElement
+    // Obtiene las dimensiones relevantes del documento
+    const { scrollHeight, clientHeight, scrollTop } = document.documentElement;
 
-    console.log(`scrollTop + clientHeight = ${scrollTop + clientHeight} | Altura personalizada = ${scrollHeight - 3}`)
-    scrollTop + clientHeight > scrollHeight - 5 && setTimeout(newContainer, 500)
-})
+    // Imprime información en la consola
+    console.log(`scrollTop + clientHeight = ${scrollTop + clientHeight} | Altura personalizada = ${scrollHeight - 3}`);
 
+    // Verifica si el desplazamiento alcanza una posición específica y luego llama a la función newContainer
+    if (scrollTop + clientHeight > scrollHeight - 10) {
+        setTimeout(newContainer, 500);
+    }
+});
+
+// Función para crear un nuevo contenedor
 const newContainer = () => {
-    const box = document.createElement('div')
-    box.className = 'box d'
-    container.appendChild(box)
-}
+    const box = document.createElement('div');
+    box.className = 'box d'; // Asigna la clase 'box d' al nuevo contenedor
+    container.appendChild(box); // Agrega el contenedor al elemento 'container'
+};
